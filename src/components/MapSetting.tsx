dc3a2ceae652e09b6fa02cd591851e40b067c565
novice0840 <magnolia0840@gmail.com>
2024-02-02 15:59:12 +0900

    feat: google map 원 그리기 성공

--- src/components/MapSetting.tsx
+++ src/components/MapSetting.tsx
@@ -3,11 +3,11 @@ import { useMap, useMapsLibrary } from "@vis.gl/react-google-maps";
 
 const MapSetting = () => {
   const map = useMap("bf51a910020fa25a");
-  const { Circle } = useMapsLibrary("maps");
+  // const { Circle } = useMapsLibrary("maps");
 
   useEffect(() => {
     if (!map) return;
-    Circle.setmap(map);
+    // Circle.setmap(map);
   }, [map]);
   return <div>This is map component;</div>;
 };
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,88 +1,50 @@
-import Layout from "../components/Layout";
-import {
-  APIProvider,
-  AdvancedMarker,
-  Map,
-  Marker,
-  Pin,
-} from "@vis.gl/react-google-maps";
-import { Circle } from "@react-google-maps/api";
-import MapSetting from "../components/MapSetting";
+import React from "react";
+// import {createRoot} from 'react-dom/client';
 
-const Test = () => {
-  const API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { Circle } from "../components/Circle";
+// import ControlPanel from './control-panel';
+
+// import {POLYGONS} from './encoded-polygon-data';
+
+const API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+
+const INITIAL_CENTER = { lat: 41.1897, lng: -96.0627 };
+
+const App = () => {
+  const [center, setCenter] = React.useState(INITIAL_CENTER);
+  const [radius, setRadius] = React.useState(43000);
+
+  const changeCenter = (newCenter: google.maps.LatLng | null) => {
+    if (!newCenter) return;
+    setCenter({ lng: newCenter.lng(), lat: newCenter.lat() });
+  };
+
   return (
-    <Layout>
-      <APIProvider apiKey={API_KEY}>
-        <div style={{ height: "500px" }}>
-          <MapSetting />
-          <Map
-            mapId={"bf51a910020fa25a"}
-            zoom={3}
-            center={{ lat: 12, lng: 0 }}
-            gestureHandling={"greedy"}
-            disableDefaultUI={true}
-          >
-            {/* simple marker */}
-            <Marker
-              position={{ lat: 10, lng: 10 }}
-              clickable={true}
-              onClick={() => alert("marker was clicked!")}
-              title={"clickable google.maps.Marker"}
-            />
-            <Circle
-              center={{ lat: 100, lng: 100 }}
-              radius={30000}
-              options={{ strokeColor: "#ff0000" }}
-            />
-
-            {/* advanced marker with customized pin */}
-            <AdvancedMarker
-              position={{ lat: 20, lng: 10 }}
-              title={"AdvancedMarker with customized pin."}
-            >
-              <Pin
-                background={"#22ccff"}
-                borderColor={"#1e89a1"}
-                glyphColor={"#0f677a"}
-              ></Pin>
-            </AdvancedMarker>
-
-            {/* advanced marker with html pin glyph */}
-            <AdvancedMarker
-              position={{ lat: 15, lng: 20 }}
-              title={"AdvancedMarker with customized pin."}
-            >
-              <Pin background={"#22ccff"} borderColor={"#1e89a1"} scale={1.4}>
-                {/* children are rendered as 'glyph' of pin */}
-                👀
-              </Pin>
-            </AdvancedMarker>
-
-            {/* advanced marker with html-content */}
-            <AdvancedMarker
-              position={{ lat: 29, lng: 10 }}
-              title={"AdvancedMarker with custom html content."}
-            >
-              <div
-                style={{
-                  width: 300,
-                  height: 300,
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  // background: "#1dbe80",
-                  border: "2px solid #0e6443",
-                  borderRadius: "50%",
-                  transform: "translate(-50%, -50%)",
-                }}
-              ></div>
-            </AdvancedMarker>
-          </Map>
-        </div>
-      </APIProvider>
-    </Layout>
+    <APIProvider apiKey={API_KEY}>
+      <div style={{ width: 500, height: 500 }}>
+        <Map zoom={10} center={INITIAL_CENTER} gestureHandling={"greedy"} disableDefaultUI={true}>
+          <Marker
+            position={center}
+            draggable
+            onDrag={(e) => setCenter({ lat: e.latLng?.lat() ?? 0, lng: e.latLng?.lng() ?? 0 })}
+          />
+          <Circle
+            radius={radius}
+            center={center}
+            onRadiusChanged={setRadius}
+            onCenterChanged={changeCenter}
+            strokeColor={"#0c4cb3"}
+            strokeOpacity={1}
+            strokeWeight={3}
+            fillColor={"#3b82f6"}
+            fillOpacity={0.3}
+            editable
+            draggable
+          />
+        </Map>
+      </div>
+    </APIProvider>
   );
 };
-
-export default Test;
+export default App;
