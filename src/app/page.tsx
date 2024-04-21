fceb0ac1a7768d8861e1529fd827804811a6b010
Kite_Z <duswl339@ajou.ac.kr>
2024-04-21 16:23:09 +0900

    fix: compile error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 'use client'
 
 import React, {useEffect, useState} from 'react'
-import Graph from "@/components/Graph";
 import Link from 'next/link';
-import {Region} from "@/type/type";
+import Graph from "../components/Graph";
+export enum Region {
+  SEOUL = "서울특별시",
+  BUSAN = "부산광역시",
+  DAEGU = "대구광역시",
+  INCHEON = "인천광역시",
+  GWANGJU = "광주광역시",
+  DAEJEON = "대전광역시",
+  ULSAN = "울산광역시",
+  SEJONG = "세종특별자치시",
+  GYEONGGI = "경기도",
+  GANGWON = "강원특별자치도",
+  CHUNGBUK = "충청북도",
+  CHUNGNAM = "충청남도",
+  JEONBUK = "전북특별자치도",
+  JEONNAM = "전라남도",
+  GYEONGBUK = "경상북도",
+  GYEONGNAM = "경상남도",
+  JEJU = "제주특별자치도",
+  UNKNOWN = "미확인"
+}
 
 export interface RegionCountInterface {
   region: Region;
